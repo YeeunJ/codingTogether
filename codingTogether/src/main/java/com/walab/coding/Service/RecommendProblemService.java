9fb1693ccba992ab44d984d98cb4610c0acc7968
KimBitnari-hgu <21700099@handong.edu>
2021-01-18 23:14:45 +0900

    recommend read

---
+++ codingTogether/src/main/java/com/walab/coding/Service/RecommendProblemService.java
@@ -0,0 +1,10 @@
+package com.walab.coding.Service;
+
+import java.util.List;
+
+import com.walab.coding.Model.RecommendDTO;
+
+public interface RecommendProblemService {
+
+	List<RecommendDTO> readRecom();
+}
