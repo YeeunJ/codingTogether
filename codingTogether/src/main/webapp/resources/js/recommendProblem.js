46b4bc52dd78e4c1b0697c9fe5a599612dfcb70f
KimBitnari-hgu <21700099@handong.edu>
2021-01-22 16:45:38 +0900

    recommend page 일부 수정

--- codingTogether/src/main/webapp/resources/js/recommendProblem.js
+++ codingTogether/src/main/webapp/resources/js/recommendProblem.js
@@ -1,16 +1,17 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
 	$('#register-button').on('click', function() {
 		 createModel("#registerRecommendProblem", "문제집 등록", addAjax);
 		 $('select').formSelect();
 	});
-});
+});*/
+
 function printAllContent(id){
-	$('#problems').html($(id+' .pProblem').html());
-	$('#tags').html($(id+' .pTag').html());
-	$('#contents').html($(id+' .pContent').html());
-	$('#recommends').html($(id+' .pRecommend').html());
+	$('#problems').html($(id+' .readProblem').html());
+	$('#tags').html($(id+' .readTag').html());
+	$('#contents').html($(id+' .readContent').html());
+	$('#recommends').html($(id+' .readRecommend').html());
 	
-	rudModel("#readRecommendProblem", "#updateRecommendProblem", $(id+' .pTitle').html(), updateAjax, deleteAjax);
+	//rudModel("#readRecommendProblem", "#updateRecommendProblem", $(id+' .readTitle').html(), updateAjax, deleteAjax);
 	$('select').formSelect();
 }
 
@@ -29,24 +30,7 @@ function deleteAjax (){
 	//ajax 넣는 함수
 }
 
-
 function deleteThis(id){
 	var allid = "#"+id;
 	$(allid).remove();
-}
-
-var count=0;
-function insertProblems(){
-	var array = ['', '백준', 'leetcode', 'SW expert academy', 'oncoder', 'goorm', 'leetcode[database]', 'link'];
-	var site = $("#siteName option:selected").val();
-	var value = document.getElementById("problems").value;
-	var valueSplit = value.split(',');
-	var data = $('#confirmSite').html();
-	for(var i in valueSplit){
-		data += '<div id = "confirmProblemValue'+count+'" onClick="deleteThis(\'confirmProblemValue'+count+'\')"><input disabled value="'+valueSplit[i]+' ('+array[site]+')" id="last_name disabled" type="text" class="validate"/></div>';
-		count++;
-	}
-	$('#confirmSite').html(data);
-	document.getElementById("problems").value = "";
-	$("#siteName option:selected").val("");
 }
